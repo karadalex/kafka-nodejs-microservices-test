d6f33c4cb52bc81b967a2ce84efa98a6d22f6610
Alexios Karadimos <karadalex@gmail.com>
2018-10-21 12:49:24 +0300

    Successfully get message from route parameter and pass it to Kafka topic

--- producer.js
+++ producer.js
@@ -9,34 +9,27 @@ var client = new Client('192.168.1.3:2181')
 var producer = new Producer(client)
 
 const port = 3000
-var payloads = [
-    {
-        topic: 'web',
-        messages: 'Web microservice started',
-        partition: 0
-    }
-]
 
 app.get('/', (req, res) => res.send('Type your message in the route/address bar'))
-app.get('/:message', (req, res) => {
-    var msg = req.params.message
-    payloads = [
-        {
-            topic: 'web',
-            messages: msg,
-            partition: 0
-        }
-    ]
-    res.send(`Your message is ${msg}`)
+producer.on('ready', function () {
+    app.get('/:message', (req, res) => {
+        var msg = req.params.message
+        var payloads = [
+            {
+                topic: 'web',
+                messages: msg,
+                partition: 0
+            }
+        ]
+        producer.send(payloads, function (err, data) {
+            console.log(data)
+        })
+        producer.on('error', function (err) {
+            console.log(err)
+        })
+
+        res.send(`Your message is ${msg}`)
+    })
 })
 
 app.listen(port, () => console.log(`Producer web microservice is running on port ${port}`))
-
-producer.on('ready', function () {
-    producer.send(payloads, function (err, data) {
-        console.log(data)
-    })
-})
-producer.on('error', function (err) {
-    console.log(err)
-})
